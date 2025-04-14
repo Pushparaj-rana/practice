ae9280cd61fcc0e1ed2f2e01f65ae9facff05c33
Pushparajsinh <rpushparaj97@gmail.com>
2025-04-14 16:22:24 +0530

    Update hungry.py

--- hungry.py
+++ hungry.py
@@ -4,6 +4,8 @@ if hungry == "yes":
     print("you must it banana")
     print("you can it pizza")
     print("eat burger")
+    print("eat apple")
+    
 else:
     print(" you must do homework")
     
