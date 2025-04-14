e5c7f9fbec11b2bba90189dc1a357a38860595d9
Rana Pushparajsinh <rpushparaj97@gmail.com>
2025-04-14 13:32:08 +0530

    pizza and burger

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ hungry = input("Are you hungry?")
 
 if hungry == "yes":
     print("you must it banana")
+    print("you can it pizza")
+    print("eat burger")
 else:
     print(" you must do homework")
     
